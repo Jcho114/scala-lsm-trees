7507ea2e1f53f17a388d7dafc45bfc0f7b3ad063
Joseph Cho <108897459+Jcho114@users.noreply.github.com>
2026-04-03 12:36:08 -0400

    chore(wal): wal can reload on restart

--- src/main/scala/WriteAheadLog.scala
+++ src/main/scala/WriteAheadLog.scala
@@ -1,7 +1,20 @@
-import java.io.{DataOutputStream, File, FileOutputStream}
+import java.io.{DataOutputStream, File, FileOutputStream, RandomAccessFile}
+import scala.collection.mutable.ArrayBuffer
 
 class WriteAheadLog(filename: String) {
+  val entries: ArrayBuffer[Entry] = ArrayBuffer.empty[Entry]
+  initEntriesFromFile(filename)
   private val file = new DataOutputStream(new FileOutputStream(filename, true))
+
+  private def initEntriesFromFile(filename: String): Unit = {
+    val tempFile = new File(filename)
+    if (!tempFile.exists()) return
+    val file = new RandomAccessFile(filename, "r")
+    while (file.getFilePointer < file.length()) {
+      val entry = ReaderWriterUtils.readNextEntry(file)
+      entries.addOne(entry)
+    }
+  }
 
   /**
    * Write entry to WAL
@@ -9,8 +22,9 @@ class WriteAheadLog(filename: String) {
    * @param value Value
    */
   def write(key: String, value: String): Unit = {
-    writeString(key)
-    writeString(value)
+    ReaderWriterUtils.writeString(file, key)
+    ReaderWriterUtils.writeString(file, value)
+    entries.addOne(Entry(key, value))
   }
 
   /**
@@ -20,15 +34,5 @@ class WriteAheadLog(filename: String) {
     file.close()
     val tempFile = new File(filename)
     if (tempFile.exists()) tempFile.delete()
-  }
-
-  /**
-   * Helper function to write a string and its length to a file
-   * @param s String to write to file stream
-   */
-  private def writeString(s: String): Unit = {
-    val bytes = s.getBytes("UTF-8")
-    file.writeInt(bytes.length)
-    file.write(bytes)
   }
 }
